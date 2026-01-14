a9c61426a42d130504801ba43d98016115bad1b0
druskus20 <43417195+druskus20@users.noreply.github.com>
2026-01-14 05:04:32 +0100

    fix: signatures were not idiomatic

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,46 @@ impl LiquidFloatComplex {
     }
 }
 
+impl From<f32> for LiquidFloatComplex {
+    fn from(value: f32) -> Self {
+        Self::new(value, 0.0)
+    }
+}
+
 pub struct DetectorCCCF {
     ptr: raw::detector_cccf,
 }
 
 impl DetectorCCCF {
-    pub fn detector_cccf_correlate(
-        &mut self,
-        x: LiquidFloatComplex,
-        tau_hat: &mut f32,
-        dphi_hat: &mut f32,
-        gamma: &mut f32,
-    ) -> i32 {
-        unsafe { raw::detector_cccf_correlate(self.ptr, x.raw, tau_hat, dphi_hat, gamma) }
+    pub fn new(k: &mut [LiquidFloatComplex], p: f32, beta: f32) -> Self {
+        let mut preample = k.iter().map(|c| c.raw).collect::<Vec<_>>();
+        let n = preample.len() as u32;
+        let ptr = unsafe { raw::detector_cccf_create(preample.as_mut_ptr(), n, p, beta) };
+        assert!(!ptr.is_null());
+        Self { ptr }
     }
+    pub fn correlate(&mut self, x: LiquidFloatComplex) -> CorrelationResult {
+        let mut tau_hat = 0.0;
+        let mut dphi_hat = 0.0;
+        let mut gamma = 0.0;
+        let r = unsafe {
+            raw::detector_cccf_correlate(self.ptr, x.raw, &mut tau_hat, &mut dphi_hat, &mut gamma)
+        };
+
+        CorrelationResult {
+            detected: r != 0,
+            tau_hat,
+            dphi_hat,
+            gamma,
+        }
+    }
+}
+
+pub struct CorrelationResult {
+    pub detected: bool,
+    pub tau_hat: f32,
+    pub dphi_hat: f32,
+    pub gamma: f32,
 }
 
 impl Drop for DetectorCCCF {
